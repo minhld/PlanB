8310885f2d7b20478aeec5b894dbdb346c7d254c
mac <mac@macs-MacBook-Pro.local>
2023-05-14 22:41:39 -0400

    rollback the circuit breaker

--- src/main/java/com/minhld/planb/controller/LoginController.java
+++ src/main/java/com/minhld/planb/controller/LoginController.java
@@ -1,13 +1,15 @@
 package com.minhld.planb.controller;
 
-import com.minhld.planb.data.object.User;
 import com.minhld.planb.model.RequestUser;
 import com.minhld.planb.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
