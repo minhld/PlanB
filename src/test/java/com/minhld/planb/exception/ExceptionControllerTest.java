0259d2714880ea27cb2a1354fc916482cb02feaa
minhld <minhld38@gmail.com>
2023-07-15 16:32:15 -0400

    added test for exception controller

---
+++ src/test/java/com/minhld/planb/exception/ExceptionControllerTest.java
@@ -0,0 +1,25 @@
+package com.minhld.planb.exception;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@SpringBootTest
+public class ExceptionControllerTest {
+    @Autowired
+    ExceptionController exceptionController;
+
+    @DisplayName("test login exception")
+    @Test
+    void testException() {
+        LoginException e = Mockito.mock(LoginException.class);
+        ResponseEntity entity = exceptionController.exception(e);
+        Assertions.assertEquals(entity.getStatusCodeValue(), HttpStatus.NOT_FOUND.value());
+    }
+}
